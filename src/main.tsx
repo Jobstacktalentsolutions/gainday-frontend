b8e99cf0d9607a0a0f7d61e2c23ab5b164222a99
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-07-28 01:46:14 +0100

    chore : configured tanstack's wrapper

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,14 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client = {queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
