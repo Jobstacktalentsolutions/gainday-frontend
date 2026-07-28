c869bb8080d8f66984f3a856fc7374050692fc8c
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-07-28 01:35:24 +0100

    chore : set up shadcn ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 
-
 function App() {
-
 
   return (
     <>
-      <h1 className="text-green-500 text-2xl font-bold">Gainday is running ...</h1>
+      <h1 className="text-green-500 text-2xl font-bold ">Gainday is running ...</h1>
     </>
   )
 }
