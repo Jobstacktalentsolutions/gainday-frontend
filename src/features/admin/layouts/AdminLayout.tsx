7e782ea2ae20d17208cc9f44d56831c7ab198c26
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-07-31 11:55:57 +0100

    feat : created the layout for the admin dashboard

--- src/features/admin/layouts/AdminLayout.tsx
+++ src/features/admin/layouts/AdminLayout.tsx
@@ -1,2 +1,15 @@
 import { Outlet } from "react-router-dom";
-import { AdminSidebar } from "@/components/AdminSidebar";
+import AdminSidebar from "../components/AdminSidebar";
+
+const AdminLayout = () => {
+    return (
+        <div className="flex min-h-screen items-start bg-neutral-50">
+            <AdminSidebar />
+            <main className = "flex flex-1 flex-col gap-6 px-10 py-8">
+                <Outlet />
+            </main>
+        </div>
+    );
+}
+
+export default AdminLayout ;
