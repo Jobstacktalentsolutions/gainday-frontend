107d62618216b79dd45780257bd039c0599ee082
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-01 18:17:00 +0100

    feat : added the landing page skeleton

---
+++ src/features/landing/pages/LandingPage.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+
+const LandingPage = () => {
+    return (
+        <div className="bg-neutral-50">
+            <Header />
+            <main className="pt-24.25">
+
+            </main>
+
+        </div>
+    );
+}
+
+export default LandingPage;
