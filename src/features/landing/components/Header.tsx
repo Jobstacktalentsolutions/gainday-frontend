fd1c221429a8b0782cc0c126c5cfdc07dbad57a4
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-01 18:12:59 +0100

    feat : completed the header component

--- src/features/landing/components/Header.tsx
+++ src/features/landing/components/Header.tsx
@@ -2,44 +2,51 @@ import { Link } from "react-router-dom";
 import brandLogo from "@/assets/gainday icon.svg";
 
 const navLinks = [
-    { label : "For employers", href: "#for-employers"},
-    { label : "For candidates", href : "for-candidates"},
-    { label : "Open Roles", href : "#open-roles"},
+    { label: "For employers", href: "#for-employers" },
+    { label: "For candidates", href: "for-candidates" },
+    { label: "Open Roles", href: "#open-roles" },
 ]
 
 
 const Header = () => {
-    return(
-        <header>
-            <Link to = "/" className = "shrink-0">
+    return (
+        <header className  = "fixed top-0 left-0 z-50 flex w-full items-center justify-between border-b-[0.5px] border-neutral-300 bg-white/10 px-6 py-5 backdrop-blur-[100px] md:px-30">
+            <Link to="/" className="shrink-0">
                 <img
-                src= {brandLogo}
-                alt = "Gainday logo"
-                className="h-12.75 w-30 md:h-[57.6px] md:w-34"
+                    src={brandLogo}
+                    alt="Gainday logo"
+                    className="h-12.75 w-30 md:h-[57.6px] md:w-34"
                 />
             </Link>
 
             <nav className="hidden items-center gap-6 md:flex">
                 {navLinks.map((link) => (
                     <a
-                    key = {link.href}
-                    href = {link.href}
-                    className = "p-2.5 font-sans text-base text-neutral-700"
+                        key={link.href}
+                        href={link.href}
+                        className="p-2.5 font-sans text-base text-neutral-700"
                     >
                         {link.label}
                     </a>
                 ))}
             </nav>
 
-            <div className = "hidden items-center gap-3 md:flex">
+            <div className="hidden items-center gap-3 md:flex">
                 <a
-                href = "#post-a-job"
-                className="flex h-13 items-center justify-center rounded-xl border border-primary-500 px-10 py-2 font-sans"
+                    href="#post-a-job"
+                    className="flex h-13 items-center justify-center rounded-xl border border-primary-500 px-10 py-2 font-sans"
                 >
                     Post a Job
                 </a>
-
+                <a
+                    href="#try-a-challenge"
+                    className="flex h-13 w-37.75 items-center justify-center rounded-xl bg-primary-500 px-10 py-2 text-base text-neutral-50"
+                >
+                    Try a challenge
+                </a>
             </div>
         </header>
     );
 }
+
+export default Header;
