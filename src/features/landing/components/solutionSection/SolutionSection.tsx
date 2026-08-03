a71efc8d1674336a0215e380aa5f2ad8130c1ee4
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-03 19:56:34 +0100

    feat : completely made the solution section desktop responsive

--- src/features/landing/components/solutionSection/SolutionSection.tsx
+++ src/features/landing/components/solutionSection/SolutionSection.tsx
@@ -1,16 +1,17 @@
 import { motion } from "motion/react";
 import GradientCard from "../GradientCard";
 import SectionTag from "../SectionTag";
+import CTAButton from "./CTAButton";
 
 const SolutionSection = () => {
     return (
         <motion.section
             className="py-25 px-6 gap-30 flex flex-col"
         >
-            <div className="flex flex-col gap-4">
-                <div >
+            <div className="flex flex-col gap-4 lg:flex-row  lg:px-30 lg:gap-44.5">
+                <div className="lg:flex-1">
                     <SectionTag label="FOR CANDIDATES" />
-                    <h3 className="text-[32px] tracking-[-0.32px] leading-9.5 pt-4">
+                    <h3 className="text-[32px] tracking-[-0.32px] leading-9.5 pt-4 lg:text-[48px] lg:tracking-[-0.48px] lg:leading-14.5">
                         Skip the CV black hole.
                         Show employers what you can actually do.
                     </h3>
@@ -20,14 +21,29 @@ const SolutionSection = () => {
                         yours gets lost before a human reads it. Gainday gives you a way to prove your thinking directly through a real task, scored fairly, seen by employers who are actually hiring.
                         Get matched with employers who've already seen what you can do.
                     </p>
+                    <div className="lg:block hidden pt-4">
+                        <CTAButton
+                            label="Try a candidate challenge"
+                            onClick={() => { }}
+                        />
+                    </div>
                 </div>
-                <GradientCard />
+                <div className="lg:flex-1 lg:w-122.75 lg:h-139.5">
+                    <GradientCard />
+                    <div className="lg:hidden pt-4">
+                        <CTAButton
+                            label="Try a candidate challenge"
+                            onClick={() => { }}
+                        />
+                    </div>
+                </div>
+
 
             </div>
-            <div className="flex flex-col gap-4 ">
-                <div>
+            <div className="flex flex-col gap-4 lg:flex-row-reverse lg:px-30 lg:gap-44.5 ">
+                <div className="lg:flex-1">
                     <SectionTag label="FOR EMPLOYERS" />
-                    <h3 className="text-[32px] tracking-[-0.32px] leading-9.5 pt-4">
+                    <h3 className="text-[32px] tracking-[-0.32px] leading-9.5 pt-4 lg:text-[48px] lg:tracking-[-0.48px] lg:leading-14.5">
                         Stop guessing.
                         Start seeing.
                     </h3>
@@ -36,10 +52,26 @@ const SolutionSection = () => {
                         You get a ranked shortlist with capability scores not a pile of CVs to wade through.
                         You see their reasoning before you spend a penny.
                     </p>
+                    <div className="lg:block hidden pt-4">
+                        <CTAButton
+                            label="Post a job now"
+                            onClick={() => { }}
+                        />
+                    </div>
 
                 </div>
 
-                <GradientCard />
+                <div className="g:flex-1 lg:w-122.75 lg:h-139.5">
+                    <GradientCard />
+                    <div className="lg:hidden pt-4">
+                        <CTAButton
+                            label="Post a job now"
+                            onClick={() => { }}
+                        />
+                    </div>
+
+                </div>
+
             </div>
 
         </motion.section>
