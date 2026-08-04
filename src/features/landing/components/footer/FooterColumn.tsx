cf9ce2edf8d55062a3ce5df03027830953032a34
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-04 22:59:15 +0100

    feat : fixed a tailwind warning

--- src/features/landing/components/footer/FooterColumn.tsx
+++ src/features/landing/components/footer/FooterColumn.tsx
@@ -5,7 +5,7 @@ interface FooterColumnProps {
 
 export function FooterColumn({ heading, links }: FooterColumnProps) {
     return (
-        <div className="flex w-20 flex-col items-start gap-4 lg:w-[200px] lg:gap-10">
+        <div className="flex w-20 flex-col items-start gap-4 lg:w-50 lg:gap-10">
             <p className="text-base text-primary-950">{heading}</p>
             <div className="flex flex-col items-start gap-3 text-base whitespace-nowrap text-neutral-700 lg:text-xs">
                 {links.map((link) => (
