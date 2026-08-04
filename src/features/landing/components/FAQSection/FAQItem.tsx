4535cd9a72b24654b1e3b11be6b07c47dfe86ed2
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-04 18:18:37 +0100

    feat : added styles for the open and closed state of the FAQ item

--- src/features/landing/components/FAQSection/FAQItem.tsx
+++ src/features/landing/components/FAQSection/FAQItem.tsx
@@ -1,8 +1,39 @@
 
+import { motion, AnimatePresence } from "motion/react";
+import { Plus, Minus} from "lucide-react";
 
-const FAQItem = () => {
+interface FAQItemProps {
+    id :string ;
+    question : string;
+    answer : string ;
+    isOpen : boolean;
+    onToggle : () => void;
+}
+
+const FAQItem = ({ id, question, answer, isOpen, onToggle} : FAQItemProps) => {
+    
+    const panelId = `faq-panel-${id}`
+    const buttonId = `faq-button-${id}`
+
+    
     return (
-        <div>
+        <div
+        className= {
+            isOpen 
+            ? "w-full rounded-xl bg-black/2 px-6 py-4 backdrop-blur-[20px] lg:rounded-3xl lg:px-10 lg:py-10"
+            : "w-full border-b border-neutral-300 px-3 py-5 lg:px-10 lg:py-10"
+        }
+        >
+            <button
+            id = {buttonId}
+            type = "button"
+            onClick = { onToggle }
+            aria-expanded={ isOpen }
+            aria-controls = { panelId }
+            className = "flex w-full items-center justify-between gap-4 text-left"
+            >
+
+            </button>
 
         </div>
     );
