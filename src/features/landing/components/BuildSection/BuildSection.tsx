24bb3184b84051208b905037613ecfc3d6204c2e
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-04 20:20:06 +0100

    feat : created  a separate start now button

--- src/features/landing/components/BuildSection/BuildSection.tsx
+++ src/features/landing/components/BuildSection/BuildSection.tsx
@@ -1,9 +1,30 @@
+import CTAButton from "../solutionSection/CTAButton";
+import StartNowButton from "./StartNowButton";
 
 const BuildSection = () => {
     return (
         <div
             className="py-25 px-6"
         >
+            <div className="bg-primary-500 text-white flex flex-col gap-3 py-12.75 px-5 rounded-[12px]">
+                <h3 className="text-[32px] leading-9.5 tracking-[-0.32px]">
+                    Built for Proof, not Paper.
+                </h3>
+                <p className="text-base leaading-6 text-neutral-200">
+                    Show employers what you can actually do not just what your
+                    CV says you've done. Try the live challenge and get on the
+                    waitlist for what's next.
+                </p>
+                <div className="h-55 bg-white w-full rounded-[12px]">
+
+                </div>
+                <div>
+                    <StartNowButton 
+                    label = "Start Now"
+                    />
+                </div>
+
+            </div>
 
         </div>
     );
---
+++ src/features/landing/components/BuildSection/StartNowButton.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@/components/ui/button"
+import arrowTr from "@/assets/arrow-tr.svg"
+
+interface CTAButtonProps {
+    label: string;
+    onClick?: () => void;
+}
+
+const StartNowButton = ({ label, onClick }: CTAButtonProps) => {
+    return (
+        <Button
+            onClick={onClick}
+            className="text-base text-info-950 h-11 flex gap-2 items-center justify-center pl-4 pr-1 py-2 bg-primary-50 hover:bg-primary-100"
+        >
+            {label}
+            <span className="w-9 h-9 bg-secondary-500 flex items-center justify-center rounded-[8px]">
+                <img
+                    src={arrowTr}
+                    alt="arrow pointing North East"
+                />
+            </span>
+        </Button>
+    );
+}
+
+export default StartNowButton;
