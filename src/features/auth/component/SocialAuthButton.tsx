02206d947f8020e4cfe05bea00944528f14f5364
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-06 11:02:15 +0100

    feat : finished the Social Auth Button

--- src/features/auth/component/SocialAuthButton.tsx
+++ src/features/auth/component/SocialAuthButton.tsx
@@ -1,9 +1,28 @@
-
+import googleIcon from "@/assets/Google.svg";
 
 interface SocialAuthButtonProps {
-    provider : "google"
-    label : string
+    label: string
     onClick?: () => void
 }
+
+const SocialAuthButton = ({ label, onClick }: SocialAuthButtonProps) => {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            className="flex h-13 w-full items-center justify-center gap-2 rounded-xl border border-primary-500 px-6 text-base text-primary-500"
+        >
+            <img
+                src={googleIcon}
+                alt="Google icon"
+                className="size-4"
+            />
+            {label}
+
+        </button>
+    )
+}
+
+export default SocialAuthButton;
 
 
