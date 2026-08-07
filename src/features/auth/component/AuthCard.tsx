671a1651eabec6cd40edd6cd0923bf8b578b27b0
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-07 09:36:07 +0100

    feat : configured the overla for the desktop backdrop(auth)

--- src/features/auth/component/AuthCard.tsx
+++ src/features/auth/component/AuthCard.tsx
@@ -11,3 +11,28 @@ interface AuthCardProps {
     open : boolean;
     onOpenChange : (open : boolean) => void;
 }
+
+const AuthCard = ({
+    title,
+    subtitle,
+    children,
+    open,
+    onOpenChange
+} : AuthCardProps) => {
+
+    const isDesktop = useMediaQuery("(min-width: 1024px)")
+
+    return (
+        <Dialog.Root open = { open } onOpenChange={ onOpenChange }>
+            <Dialog.Portal>
+                <Dialog.Overlay
+                className = {cn("fixed inset-0 z-50",
+                    isDesktop && "bg-primary-200/50 backdrop-blur-[20px]"
+                )}
+                />
+
+                
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
+}
