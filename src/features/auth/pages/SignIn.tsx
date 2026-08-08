a10f61420afed706acb588d04e4b1c522c0779ff
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-08 22:33:44 +0100

    feat : specified the imports for the SignIn page

---
+++ src/features/auth/pages/SignIn.tsx
@@ -0,0 +1,15 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useNavigate, Link } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { apiClient } from "@/lib/api/client";
+import { ActionButton } from "@/components/ui/ActionButton";
+import { FormInput } from "@/components/form/FormInput";
+import { AuthDivider } from "../component/AuthDivider";
+import SocialAuthButton from "../component/SocialAuthButton";
+import AuthSwitchLink from "../component/AuthSwitchLink";
+import { signInSchema, type signInFormValues } from "../schemas/signInSchema";
+
+
+
+
