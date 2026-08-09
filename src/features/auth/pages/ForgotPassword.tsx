dfef2d230051041278fa41f8412a869b7e46315f
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-09 08:50:21 +0100

    feat : configured the handlers for the forgotPassword page

--- src/features/auth/pages/ForgotPassword.tsx
+++ src/features/auth/pages/ForgotPassword.tsx
@@ -9,3 +9,27 @@ import { ActionButton } from "@/components/ui/ActionButton";
 import { FormInput } from "@/components/form/FormInput";
 import AuthCard from "../component/AuthCard";
 import { forgetPasswordSchema, type forgetPasswordFormValues } from "../schemas/forgetPasswordSchema";
+
+
+const ForgotPassword = () => {
+    const navigate = useNavigate() 
+    const [sentTo, setSentTo] = useState<string | null>(null);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<forgetPasswordFormValues>({ resolver : zodResolver(forgetPasswordSchema)});
+
+    const  requestResetMutation = useMutation({
+        mutationFn : (values : forgetPasswordFormValues) =>
+            apiClient.post("/auth/request-password-reset", values),
+        onSuccess : (_res, values) => setSentTo(values.email),
+    })
+
+    return (
+
+    );
+}
+
+export default ForgotPassword;
