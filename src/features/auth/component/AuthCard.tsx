7f92bc916c72628d03da00c1bb473325a7941380
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-10 13:04:39 +0100

    feat : added the header to the AuthCard

--- src/features/auth/component/AuthCard.tsx
+++ src/features/auth/component/AuthCard.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import brandLogo from "@/assets/gainday.svg"
+import Header from "@/features/landing/components/Header"
 
 interface AuthCardProps {
     title: string;
@@ -25,49 +26,52 @@ const AuthCard = ({
     const isDesktop = useMediaQuery("(min-width: 1024px)")
 
     return (
-        <Dialog.Root open={open} onOpenChange={onOpenChange}>
-            <Dialog.Portal>
-                <Dialog.Overlay
-                    className={cn("fixed inset-0 z-50",
-                        isDesktop && "bg-primary-200/50 backdrop-blur-[20px]"
-                    )}
-                />
-                <Dialog.Content
-                    onOpenAutoFocus={(e) => {
-                        if (!isDesktop) e.preventDefault();
-                    }}
-                    className={cn(
-                        "fixed z-50 flex flex-col items-center gap-12 bg-white focus:outline-none lg:gap-15",
-                        isDesktop ?
-                            "top-1/2 left-1/2 w-141.5 max-w-[92vw] -translate-x-1/2 -translate-y-1/2 rounded-2xl p-12 shadow-lg"
-                            : "inset-0 w-full overflow-y-auto p-6"
-                    )}
-                >
-                    <div className="flex flex-col items-center gap-4 text-center pt-6">
-                        <div className="mb-5 lg:hidden">
-                            <img
-                                src={brandLogo}
-                                alt="Brand logo"
-                                className=""
-                            />
+        <>
+            {isDesktop && <Header />}
+            <Dialog.Root open={open} onOpenChange={onOpenChange}>
+                <Dialog.Portal>
+                    <Dialog.Overlay
+                        className={cn("fixed inset-0 z-40",
+                            isDesktop && "bg-primary-200/50 backdrop-blur-[20px]"
+                        )}
+                    />
+                    <Dialog.Content
+                        onOpenAutoFocus={(e) => {
+                            if (!isDesktop) e.preventDefault();
+                        }}
+                        className={cn(
+                            "fixed z-40 flex flex-col items-center gap-12 bg-white focus:outline-none lg:gap-15",
+                            isDesktop ?
+                                "top-1/2 left-1/2 w-141.5 max-w-[92vw] -translate-x-1/2 -translate-y-1/2 rounded-2xl p-12 shadow-lg"
+                                : "inset-0 w-full overflow-y-auto p-6"
+                        )}
+                    >
+                        <div className="flex flex-col items-center gap-4 text-center pt-6">
+                            <div className="mb-5 lg:hidden">
+                                <img
+                                    src={brandLogo}
+                                    alt="Brand logo"
+                                    className=""
+                                />
+                            </div>
+                            <div className="lg:max-w-103 space-y-2">
+                                <Dialog.Title className="text-2xl leading-8 tracking-[-0.32px] text-primary-950 lg:text-[32px]">
+                                    {title}
+                                </Dialog.Title>
+                                <Dialog.Description className="text-base text-neutral-700 leading-6">
+                                    {subtitle}
+                                </Dialog.Description>
+                            </div>
+
                         </div>
-                        <div className="lg:max-w-103 space-y-2">
-                            <Dialog.Title className="text-2xl leading-8 tracking-[-0.32px] text-primary-950 lg:text-[32px]">
-                                {title}
-                            </Dialog.Title>
-                            <Dialog.Description className="text-base text-neutral-700 leading-6">
-                                {subtitle}
-                            </Dialog.Description>
-                        </div>
+                        <div className="flex w-full flex-col gap-4">{children}</div>
 
-                    </div>
-                    <div className="flex w-full flex-col gap-4">{children}</div>
-
-                </Dialog.Content>
+                    </Dialog.Content>
 
 
-            </Dialog.Portal>
-        </Dialog.Root>
+                </Dialog.Portal>
+            </Dialog.Root>
+        </>
     )
 }
 
