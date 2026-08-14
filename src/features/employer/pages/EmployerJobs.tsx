fb4c2db7640f3e3198a5692b38208b7a04f124e4
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-14 15:55:42 +0100

    feat : set up a useMemo to occasionally fetch jobs

--- src/features/employer/pages/EmployerJobs.tsx
+++ src/features/employer/pages/EmployerJobs.tsx
@@ -5,3 +5,14 @@ import JobCard, { JobCardSkeleton } from "../components/JobCard";
 import JobsEmptyState from "../components/JobsEmptyState";
 import { useEmployerJobs } from "../hooks/useEmployerJobs";
 import type { Job , JobStatusFilter} from "../types/job";
+
+const EmployerJobs = () => {
+    const { data: jobs, isLoading} = useEmployerJobs();
+    const [statusFilter, setStatusFilter] = useState<JobStatusFilter>("all");
+
+    const filteredJobs = useMemo( () => {
+        if (!jobs) return [];
+        if (statusFilter === "all") return jobs;
+        return jobs.filter((job) => job.status === statusFilter);
+    }, [jobs,statusFilter]);
+}
