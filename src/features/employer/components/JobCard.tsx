6a669c08cd44f131be2508c314391b823505b30c
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-14 13:11:44 +0100

    feat : speciified the imports for the JobCard

---
+++ src/features/employer/components/JobCard.tsx
@@ -0,0 +1,6 @@
+import { Users, ArrowRight } from "lucide-react";
+import { cn } from "@/lib/utils";
+import Skeleton from "@/components/ui/skeleton";
+import StatusBadge from "./StatusBadge";
+import JobMetaRow from "./JobMetaRow";
+import type { Job } from "../types/job";
