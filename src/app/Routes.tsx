d9ca4cd0a4f32dadd7072863cfe2f973c65b387b
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-15 23:02:57 +0100

    feat : refactored some stuff

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -22,7 +22,7 @@ const AppRoutes = () => {
                     <Route path="dashboard" element={<AdminDashboard />} />
                 </Route>
                 <Route path="/landing" element={<LandingPage />} />
-                <Route path="/employer">
+                <Route path="/employ">
                     <Route path="signup" element={<CreateAccount />} />
                     <Route path="signin" element={<SignInPage />} />
                     <Route path="forgot-password" element={<ForgotPassword />} />
