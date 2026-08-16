af02993636f2bfc8692f524d34947f4f99f36b5c
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-16 11:34:30 +0100

    feat : specified the imports for the topNav (employer)

---
+++ src/features/employer/components/EmployerTopNav.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { Bell, Menu } from "lucide-react";
+import EmployerNavDrawer from "./EmployerNavDrawer";
+import BrandLogo from "@/assets/gainday.svg";
+
+
