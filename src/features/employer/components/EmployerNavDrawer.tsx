9ffe889869a5f1398d494719b1bc8df73f775f9a
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-16 11:16:24 +0100

    feat : specified the props of the nav drawer component

--- src/features/employer/components/EmployerNavDrawer.tsx
+++ src/features/employer/components/EmployerNavDrawer.tsx
@@ -2,3 +2,21 @@ import * as Dialog from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
 import { NavLink } from "react-router-dom"
 import { cn } from "@/lib/utils"
+
+
+interface EmployerNavDrawerProps {
+    open : boolean;
+    onOpenChange : ( open : boolean ) => void;
+}
+
+const NAV_ITEMS = [
+    { to : "/employer/jobs", label : "Your jobs"}
+]
+
+const EmployerNavDrawer = ({ open, onOPenChange } : EmployerNavDrawerProps) => {
+    return (
+
+    );
+}
+
+export default EmployerNavDrawer;
