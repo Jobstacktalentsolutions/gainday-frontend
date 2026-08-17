4de323010804e4723d842b41eb28b4c8d89f15f2
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-17 12:44:52 +0100

    feat : completed the basic schema (employer)

--- src/features/employer/schemas/jobPosting.ts
+++ src/features/employer/schemas/jobPosting.ts
@@ -1,1 +1,24 @@
 import { z } from "zod";
+
+export const jobDetailsSchema = z.object({
+    title : z.string().min(3, "Job title is required"),
+    roleCategory : z.string().min(1, "Select a role category"),
+    skillLevel : z.string().min(1, "Select a skill level"),
+    company: z.string().min(1),
+    location : z.string().min(1, "Location is required"),
+    employmentType : z.string().min(1,  "Select an employment type"),
+    isRemoteFriendly : z.boolean().default(false),
+    salaryFrom : z.coerce.number().positive().optional(),
+    salaryTo : z.coerce.number().positive().optional(),
+    companyDescription : z.string().optional(),
+    roleDescription : z.string().optional(),
+    skills : z.array(z.string()).default([]),
+    simulationBrief : z
+    .string()
+    .min(40, "Give at least 40 characters so Gainday has enough to work with")
+    .max(350, "keep it under 350 characters"),
+    estimatedCompletionTime : z.string().min(1, "Select an estimated time"),
+    aiUsePolicy : z.string().min(1, "Select an AI use policy"),
+})
+
+
