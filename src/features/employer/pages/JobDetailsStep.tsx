104c9043546a685c002f06c0d17dabb07aff2cb0
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-19 06:28:35 +0100

    feat : specified necessary imports

---
+++ src/features/employer/pages/JobDetailsStep.tsx
@@ -0,0 +1,6 @@
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { useFormContext } from "react-hook-form";
+import { ArrowRight, Sparkles } from "lucide-react";
+import TagInput from "@/components/ui/tagInput";
+import JobPostingStepIndicator from "../components/JobPostingStepIndicator";
+import type { JobPostingFormValues } from "../schemas/jobPosting";
