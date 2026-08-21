4fe6c7ea9d570874d99717362d04ae001ade4105
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-21 20:12:56 +0100

    feat : created the boilerplate for the simulation builder

--- src/features/employer/pages/SimulationBuilder.tsx
+++ src/features/employer/pages/SimulationBuilder.tsx
@@ -1,2 +1,14 @@
 import { useNavigate } from "react-router-dom"
 import JobPostingStepIndicator from  "../components/JobPostingStepIndicator";
+
+const SimulationBuilder = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default SimulationBuilder;
