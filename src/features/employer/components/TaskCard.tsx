20339ce6a55835578099dfd0ce9070c8aa35dce1
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-22 21:06:18 +0100

    feat : specified the imports for the TaskCard

---
+++ src/features/employer/components/TaskCard.tsx
@@ -0,0 +1,5 @@
+import { Trash2 } from "lucide-react";
+import { FormInput } from "@/components/form/FormInput";
+import { FormTextarea } from "@/components/form/FormTextarea";
+import TaskTypeBadge from "./TaskTypeBadge";
+import type { SimulationTask } from "../schemas/jobPosting";
