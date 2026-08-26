8067923f3474e938e07f132bb168ee17caaa9f94
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-26 21:16:22 +0100

    feat : created the basic interface of usersTable

--- src/features/admin/components/UsersTable.tsx
+++ src/features/admin/components/UsersTable.tsx
@@ -1,1 +1,10 @@
-import AdminButton
+import { AdminButton } from "@/components/ui/AdminButton";
+import StatusBadge from "./StatusBadge";
+import type { AdminUser } from "../types/user";
+
+interface UsersTableProps {
+    users: AdminUser[];
+    onEdit: (user: AdminUser) => void;
+    onSuspend: (user: AdminUser) => void;
+    isSuspending: boolean;
+}
