f47a8720b1014e73843be9c1793bbb55950fde21
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-28 20:12:50 +0100

    feat : made the job cards into a grid also on desktop

--- src/features/employer/pages/EmployerJobs.tsx
+++ src/features/employer/pages/EmployerJobs.tsx
@@ -70,7 +70,7 @@ const EmployerJobs = () => {
                             ))}
                         </div>
                     ) : hasJobs ? (
-                        <div className="flex w-full flex-col gap-4">
+                        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                             {filteredJobs.map((job) => (
                                 <JobCard
                                     key={job.id}
