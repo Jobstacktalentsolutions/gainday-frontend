d55b1037b8ba5e6606580b3641f66017abb28f46
Ude Daniel <dude243485@stu.ui.edu.ng>
2026-08-28 12:33:41 +0100

    scaffolded the Content management page

---
+++ src/features/admin/pages/ContentModeration.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { useAdminJobs, useRemoveJobPost } from "../hooks/useAdminJobs";
+import LiveJobPostsPanel from "../components/LiveJobPostPanel";
+import RemoveJobPostDialog from "../components/RemoveJobPostDialog";
+import type { AdminJob } from "../types/job";
+
+
+const ContentModeration = () => {
+
+    return();
+}
+
+export default ContentModeration;
